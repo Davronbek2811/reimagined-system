09091de7ce14995bc6629a7d41c5bf9a32315a89
Davronbek2811 <davronjon176@gmail.com>
2025-08-25 14:45:18 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+let h1 =  document.getElementById("h1")
+function darkmode() {
+    document.body.style.background = "black";
+    document.body.style.color = "#FFFFFF";
+    h1.style.color = "#FFF";
+    h2.style.color = "#FFF";
+    h3.style.color = "#FFF";
+    h4.style.color = "#FFF";
+    h5.style.color = "#FFF";
+    h6.style.color = "#FFF";
+    dark.style.backgroundColor = "#FB982F";
+    light.style.backgroundColor = "#C4B5A5";
+    dark.style.color = "#000";
+    light.style.color = "#fff";
+
+}
+
+function LightMode() {
+    document.body.style.backgroundColor = "#C4B5A5"
+    h1.style.color = "#000";
+    h2.style.color = "#000";
+    h3.style.color = "#000";
+    h4.style.color = "#000";
+    h5.style.color = "#000";
+    h6.style.color = "#000";
+    dark.style.backgroundColor = "#000000";
+    light.style.backgroundColor = "#FB982F";
+    dark.style.color = "#FFF";
+    light.style.color = "#000";
+
+}
